c0f59a0865d7dfd8d3637c4e62b7a4266243deb6
Rawipong0000 <109149692+Rawipong0000@users.noreply.github.com>
2022-07-18 16:33:01 +0700

    Update index

--- app.js
+++ app.js
@@ -3,14 +3,30 @@ const chalk = require('chalk')
 const debug = require('debug')('app');
 const morgan = require('morgan');
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT;
+const path = require('path');
+const productRouter = express.Router();
 
 app.use(morgan('combined'))
+app.use(express.static(path.join(__dirname,"/public/")));
+app.set("views","./src/views");
+app.set("view engine", "ejs");
+
+productRouter.route("/").get((req,res) => {
+    res.send("Im Product");
+});
+
+productRouter.route("/1").get((req,res) => {
+    res.send("Im Product1");
+});
+
+app.use("/prodocts",productRouter);
 
 app.get("/",(req,res) =>{
-    res.send('Hello World');
+    res.render('index',{username: 'Rawipong', customer:["A","B","c"]});
 })
 
-app.listen(port, ()=>{
-    debug("listening port" + chalk.green(port));
+
+app.listen(PORT, ()=>{
+    debug("listening port" + chalk.green(PORT));
 })
