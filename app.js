0274dabead687bc28672a4faa9afc2faf3136fb8
Rawipong0000 <109149692+Rawipong0000@users.noreply.github.com>
2022-07-25 19:21:01 +0700

    Mongo Connect

--- app.js
+++ app.js
@@ -6,11 +6,32 @@ const app = express();
 const PORT = process.env.PORT;
 const path = require('path');
 const productsRouter = require('./src/router/productsRouter');
+const {MongoClient} = require('mongodb');
 
 app.use(morgan('combined'))
 app.use(express.static(path.join(__dirname,"/public/")));
 app.set("views","./src/views");
 app.set("view engine", "ejs");
+
+async function main(){
+    const uri = "mongodb+srv://admin:OKAbkr71298@node34452-env-8320495.th1.proen.cloud:11405/MyProject?retryWrites=true&w=majority";
+ 
+
+    const client = new MongoClient(uri);
+ 
+    try {
+        await client.connect();
+ 
+        await  listDatabases(client);
+ 
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
+}
+
+main().catch(console.error);
 
 app.use("/products",productsRouter);
 
@@ -22,3 +43,10 @@ app.get("/",(req,res) =>{
 app.listen(PORT, ()=>{
     debug("listening port" + chalk.green(PORT));
 })
+
+async function listDatabases(client){
+    databasesList = await client.db().admin().listDatabases();
+ 
+    console.log("Databases:");
+    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
